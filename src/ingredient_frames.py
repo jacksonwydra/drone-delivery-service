bd6b19edc28d49db4f2e0c39e0cd1b3f148b3514
Jackson Wydra <jacksonwydra@gmail.com>
2022-12-12 18:31:38 -0500

    [21] remove_ingredient

--- src/ingredient_frames.py
+++ src/ingredient_frames.py
@@ -11,6 +11,9 @@ def make_ingredient_nav(parent, controller):
     
     add_ingredient_button = make_nav_button(nav, controller, AddIngredientFrame, 'Add', parent)
     add_ingredient_button.grid(row=0, column=1, sticky='w')
+    
+    remove_ingredient_button = make_nav_button(nav, controller, RemoveIngredientFrame, 'Remove', parent)
+    remove_ingredient_button.grid(row=0, column=2, sticky='w')
     make_header(nav, 'Ingredients')
     return nav
 
@@ -59,4 +62,44 @@ class AddIngredientFrame(tk.Frame):
         self.barcode.delete(0, 'end')
         self.iname.delete(0, 'end')
         self.weight.delete(0, 'end')
+        
+        
+class RemoveIngredientFrame(tk.Frame):
+    def __init__(self, parent, controller, **kwargs):
+        tk.Frame.__init__(self, parent)
+        
+        nav = tk.Frame(self)
+        self.back_button = make_nav_button(nav, controller, DisplayViewFrame, 'Back', self,
+                                      data=rse.display_ingredient_view, nav=make_ingredient_nav)
+        self.back_button.grid(row=0, column=0, sticky='w')
+        
+        make_header(nav, 'Remove Ingredient')
+        nav.pack(padx=5, pady=5, fill='both', expand=True)
+        
+        panel = tk.Frame(self)
+        self.barcode = make_entry(panel, 'Barcode', 0)
+        self.barcode.focus_set()
+        
+        submit_button = tk.Button(panel, text='Submit', command=self.submit)
+        submit_button.grid(row=1, column=0, sticky='w')
+        clear_button = tk.Button(panel, text='Clear', command=self.clear)
+        clear_button.grid(row=1, column=1, sticky='w')
+        self.error = tk.StringVar()
+        self.error_box = tk.Label(panel, textvariable=self.error, fg='red')
+        panel.pack(padx=5, pady=5, fill='both', expand=True)
+        
+        
+    def submit(self):
+        error = rse.remove_ingredient((
+            self.barcode.get(),
+        ))
+        if error:
+            self.error.set(error)
+            self.error_box.grid(row=2, column=0, columnspan=3, sticky='w')
+        else:
+            self.back_button.invoke()
+        
+        
+    def clear(self):
+        self.barcode.delete(0, 'end')
         
